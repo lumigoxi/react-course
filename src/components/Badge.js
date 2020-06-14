885be236cf7d29a280370b4350dc4eaff6a2077e
Miguel Xiap <lumigoxi@gmail.com>
2020-06-13 23:27:27 -0600

    my first component

---
+++ src/components/Badge.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import confLogo from '../images/badge-header.svg';
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+        <div>
+          <img src="" alt="Avatar" />
+          <h1>
+            Miguel <br />
+            Xiap
+          </h1>
+        </div>
+
+        <div>
+          <h3>Student of platzi</h3>
+          <p>@lumigoxi</p>
+        </div>
+
+        <div>#platziconf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
