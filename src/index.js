9bd47de2f403bc2b8632b5b2e7cf7e2f013b43ff
Miguel Xiap <lumigoxi@gmail.com>
2020-06-22 23:53:37 -0600

    Shomethings fixed again xd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Register from './pages/Register';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 import App from './components/App';
